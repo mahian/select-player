9e216dd1ccf89f1b8d32ae9dd019e34c10cd3aea
mahian ahmad <mahianahamd9@gmail.com>
2022-08-22 14:37:29 +0600

    add calculation logig

---
+++ js/calculation.js
@@ -0,0 +1,23 @@
+// calculate
+function getInputValue(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputValueStr = inputField.value;
+    const inputValue = parseInt(inputValueStr);
+    inputField.value = '';
+    return inputValue;
+}
+document.getElementById('calculate').addEventListener('click', function(){
+    const perPlayer = getInputValue('per-player-cost');
+    const playerExpensesField = document.getElementById('player-expenses');
+    playerExpensesField.innerText = perPlayer * 5;
+});
+document.getElementById('calculate-total').addEventListener('click', function(){
+    const managerCost = getInputValue('manager-cost');
+    const coachCost = getInputValue('coach-cost');
+    // player cost field
+    const playerCostField = document.getElementById('player-expenses');
+    const playerCost = parseInt(playerCostField.innerText);
+    // total cost field
+    const totalCostField = document.getElementById('total-cost');
+    totalCostField.innerText = playerCost + managerCost + coachCost;
+});
